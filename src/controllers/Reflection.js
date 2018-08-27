c5c2de816ed88b90833674ca4e21d41b824986f2
alexHooper <alexmohack@gmail.com>
2018-08-27 08:19:59 +0530

    add logic for create method using models

---
+++ src/controllers/Reflection.js
@@ -0,0 +1,13 @@
+import ReflectionModel from '../models/Reflection';
+
+const Reflection = {
+	create (req, res) {
+		if (!req.body.success && !req.body.lowPoint && !req.body.takeAway) {
+			return res.status(400).send({'message': 'All fields are required'})
+		}
+
+		const reflection = ReflectionModel.create(req.body);
+		return res.status(200).send(reflection);
+	}
+
+}
