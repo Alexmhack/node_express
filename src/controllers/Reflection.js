c4e4499e04dda008196abcef2d1d585ac6a722a1
alexHooper <alexmohack@gmail.com>
2018-08-27 08:27:14 +0530

    create endpoints for getting one and all reflections

--- src/controllers/Reflection.js
+++ src/controllers/Reflection.js
@@ -8,6 +8,19 @@ const Reflection = {
 
 		const reflection = ReflectionModel.create(req.body);
 		return res.status(201).send(reflection);
+	},
+
+	getAll(req, res) {
+		const reflections = ReflectionModel.findAll();
+		return res.status(200).send(reflections);
+	},
+
+	getOne(req, res) {
+		const reflection = ReflectionModel.findOne(req.params.id);
+		if (!reflection) {
+			res.status(400).send({'message': 'reflection not found'})
+		}
+		return res.status(200).send(reflection);
 	}
 
 }
