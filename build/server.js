9b0e6685db282d81442105b3396fba2e05302272
alexHooper <alexmohack@gmail.com>
2018-08-27 09:41:41 +0530

    build server for api endpoints

--- build/server.js
+++ build/server.js
@@ -4,6 +4,10 @@ var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _Reflection = require('./src/controllers/Reflection');
+
+var _Reflection2 = _interopRequireDefault(_Reflection);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
@@ -15,6 +19,12 @@ app.get('/', function (req, res) {
 		'message': 'YAY! Congratulations your first endpoint is working!'
 	});
 });
+
+app.post('/api/v1/reflections', _Reflection2.default.create);
+app.get('/api/v1/reflections', _Reflection2.default.getAll);
+app.get('/api/v1/reflections/:id', _Reflection2.default.getOne);
+app.put('/api/v1/reflections/:id', _Reflection2.default.update);
+app.delete('/api/v1/reflections/:id', _Reflection2.default.delete);
 
 app.listen(3000);
 console.log('APP IS RUNNING ON PORT ', 3000);
