eafac7d6cf2ab727402f362c505a4f0e7ec01dde
alexHooper <alexmohack@gmail.com>
2018-08-29 16:50:04 +0530

    add code for creating tables

--- db.js
+++ db.js
@@ -10,3 +10,25 @@ const pool = new Pool({
 pool.on('connect', () => {
 	console.log('CONNECTED TO THE DB');
 });
+
+// creating tables
+const createTables = () => {
+	const queryText = 
+		`CREATE TABLE IF NOT EXISTS
+			reflections(
+				id UUID PRIMARY KEY,
+				success VARCHAR(128) NOT NULL,
+				low_point VARCHAR(128) NOT NULL,
+				take_away VARCHAR(128) NOT NULL,
+				created_date TIMESTAMP,
+				modified_date TIMESTAMP
+			)`
+
+	pool.query(queryText).then((res) => {
+		console.log(res);
+		pool.end();
+	}).catch((err) => {
+		console.log(err);
+		pool.end();
+	})
+}
